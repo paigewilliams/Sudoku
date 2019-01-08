bce7fe8283c9772f1a3035577ff79cf3b264f689
Paige Williams and Rohan Moore <rohan@>
2019-01-08 11:40:48 -0800

    Add src files.

---
+++ src/game.js
@@ -0,0 +1,3 @@
+export function Game(board) {
+  this.board = board
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Game } from './../src/game.js';
+import './styles.css'
